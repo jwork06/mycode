331dab465fb3921ebdad488617ab6b3feebf547d
julia <jwork06@users.noreply.github.com>
2021-10-18 19:01:23 +0000

    mixed lists

---
+++ lab10challenge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""    
+
+def main():
+
+    name = input("What is your name? ")
+    day = input("What day of the week is it? ")
+    
+    print("Hello, " + name + "! Happy "+ day+"!")
+    
+
+main()
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+#prints IP address
+print("The first item in the list (IP): " + my_list[0] )
+#prints the port number
+print("The second item in the list (port): " + str(my_list[1]) )
+#prints the state
+print("The last item in the list (state): " + my_list[2] )
+print(f"IP addresses: {iplist[3]} and {iplist[4]}")
